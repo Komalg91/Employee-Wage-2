af3e287d61ecbeec27ecfc0cc984f600e9743fb0
Komal Ghanwat <kggk9050@gmail.com>
2021-09-16 09:54:19 +0530

    UC3_daily_wage

--- index.ts
+++ index.ts
@@ -15,5 +15,29 @@ function attendance_check(empCheck) {
     return empCheck;
 }
 
+const full_time = 8;
+const part_time = 4;
+const wage_per_hour = 20;
+let emp_hours;
+function daily_wage_func(random_time)
+{
+    switch(random_time)
+    {
+        case 1:
+            emp_hours = full_time;
+            break;
+        case 0:
+            emp_hours = part_time;
+            break;
+        default:
+            emp_hours = 0;
+            break;    
+    }
+    return emp_hours;
+}
+let random_time = Math.floor(Math.random()*10)%3;
+emp_hours = daily_wage_func(random_time);
+let daily_wage = emp_hours *  wage_per_hour;
+console.log("Wage: " +daily_wage);
 let emp_atd_check = Math.floor(Math.random()*10)%2;
 console.log(attendance_check(emp_atd_check));
